31c4e65fecbd2b36ac7fc603fe52d16e4989df7d
Michael Glass <me@mike.is>
2022-10-11 17:59:39 +0200

    add a variety of tests

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -8,4 +8,10 @@ describe 'Example' do
   it 'is a flake' do
     expect(rand).to be > 0.1
   end
+
+  it 'is slow' do
+    sleep(1)
+    # isn't this an odd test?
+    expect(1).to be_odd
+  end
 end
