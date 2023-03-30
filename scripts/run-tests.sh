fb56649b2c80d922123ae21cc9b665fe0fdc930e
Kyle Thompson <me@kkt.dev>
2023-03-30 12:35:18 -0400

    Use the config file for configuration

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -8,10 +8,4 @@ script_path="$(dirname -- "${0}")"
 # shellcheck source=set-captain-env-from-git.sh
 source "${script_path}/set-captain-env-from-git.sh"
 
-captain run \
-  --suite-id captain-examples-rspec \
-  --test-results tmp/rspec.json \
-  -- \
-  bundle exec rspec \
-  --format json --out tmp/rspec.json \
-  --format progress
+captain run captain-examples-rspec
