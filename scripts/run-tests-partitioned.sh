5f7c18c9c27f7d62c5e2306dcc4daecd3217ebf6
Michael Glass <me@mike.is>
2023-04-06 11:31:17 +0200

    when using an env var, don't set suite-id

--- scripts/run-tests-partitioned.sh
+++ scripts/run-tests-partitioned.sh
@@ -8,13 +8,13 @@ script_path="$(dirname -- "${0}")"
 # shellcheck source=set-captain-env-from-git.sh
 source "${script_path}/set-captain-env-from-git.sh"
 
-suite_id="captain-examples-rspec"
+export CAPTAIN_SUITE_ID=captain-examples-rspec
+
 test_files=$(
   captain partition \
-    --suite-id $suite_id \
     --index "$CAPTAIN_PARTITION_INDEX" \
     --total "$CAPTAIN_PARTITION_TOTAL" \
     "spec/**/*_spec.rb"
 )
 
-captain run $suite_id -- $test_files
+captain run -- "$test_files"
